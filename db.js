399efa546b744192075aa9a74eefeb4b090ff326
Mirko Srbinovski <srbinovskimirko@gmail.com>
2021-12-18 17:20:13 +1000

    changes to the db.js file to get to the aws not local

--- db.js
+++ db.js
@@ -1,9 +1,5 @@
 const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
-const client = new DynamoDBClient({
-    region:"ap-southeast-2",
-    accessKeyId: "access_key_id",
-    secretAccessKeyId: "secret_access_key_id",
-    endpoint: "http://locahost:8000",
+const client = new DynamoDBClient({    
 });
 
 module.exports = client;
